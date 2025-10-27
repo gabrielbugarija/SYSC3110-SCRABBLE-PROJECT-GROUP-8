c4c7ca58d014cd9d507f38534b1d18d0023d1861
mosbo <markosborne@cmail.carleton.ca>
2025-10-27 19:25:18 -0400

    Still need to fix ownsTile()

--- src/Player.java
+++ src/Player.java
@@ -1,26 +1,41 @@
 /// @author Kemal Sogut - 101280677
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player {
 
     private String name;
-    private int score;
+    private int totalScore;
     private ArrayList<Tile> rack;
+
 
     public Player(String name){
         this.name = name;
-        this.score = 0;
+        this.totalScore = 0;
         this.rack = new ArrayList<>();
     }
 
     public String getName(){
-
         return this.name;
     }
 
-    public ArrayList<Tile> getRack(){
+    public ArrayList<Tile> getRackTiles(){
         return this.rack;
+    }
+
+    public int getScore(){
+        return this.totalScore;
+    }
+
+    public void increaseScore(int points){
+        this.totalScore += points;
+    }
+
+    public void addTiles(List<Tile> tiles) {
+        if (tiles != null) {
+            this.rack.addAll(tiles);
+        }
     }
 
     public void removeTiles(List<Tile> tiles) {
@@ -29,29 +44,32 @@ public class Player {
         }
     }
 
-        while(rack.size()<7) {
+    public boolean ownsTile(String word, List<PlacedTile> placements){
+
+    }
+
+    public void fillRack(TileBag tileBag){
+        int neededTiles = 7 - rack.size();
+        if(neededTiles > 0){
+            drawTiles(tileBag, neededTiles);
+        }
+    }
+
+    private void drawTiles(TileBag tileBag, int neededTiles) {
+        for (int i = 0; i < neededTiles && tileBag.getNumberOfTilesLeft() > 0; i++) {
             rack.add(tileBag.drawTile());
         }
     }
 
-    public void printRack(){
-        for(int i=0;i<rack.size();i++){
+    public void printPlayerTiles(){
+        for(int i = 0; i< rack.size(); i++){
             Tile tile = rack.get(i);
             System.out.println(tile.getLetter()+": "+tile.getPoints());
         }
-    }
-
-    public void updateScore(int scoreToAdd){
-        score += scoreToAdd;
-    }
-
-    public int getScore(){
-        return score;
     }
 
     @Override
     public String toString() {
         return "\nPlayer Name: "+ this.name + " Score: "+this.totalScore +" \n";
     }
-
 }
