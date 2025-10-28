9863db5a89018015657631f47ab9d703643cfd04
random <randomhygaard@cmail.carleton.ca>
2025-10-27 22:22:22 -0400

    Updated test classes

--- tests/GameTest.java
+++ tests/GameTest.java
@@ -1,20 +1,46 @@
 import org.junit.Test;
-
 import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTest {
-    @Test
-    public void testGetNumOfPlayers(){
-        Board board = new Board();
-        Game game = new Game(new Board());
-        String playerNum = "3";
-        InputStream in = new ByteArrayInputStream(playerNum.getBytes());
-        System.setIn(in);
-        game.getNumberOfPlayers();
+//    @Test
+    //runs forever
+//    public void testGetNumOfPlayers(){
+//        Scanner scanner = new Scanner(System.in);
+//        Board board = new Board();
+//        Game game = new Game(new Board());
+//        String playerNum = "3";
+//        InputStream in = new ByteArrayInputStream(playerNum.getBytes());
+//        System.setIn(in);
+//        assertEquals(3, game.getNumberOfPlayers(scanner));
+//    }
 
-    }
+//    @Test
+//    public void testGetNumOfPlayersError(){
+//        Scanner scanner = new Scanner(System.in);
+//        Board board = new Board();
+//        Game game = new Game(new Board());
+//        String playerNum = "5";
+//        InputStream in = new ByteArrayInputStream(playerNum.getBytes());
+//        System.setIn(in);
+//        //need to get print output here
+//        //assertEquals("The number of player should be between 2 and 4", game.getNumberOfPlayers(scanner));
+//    }
+
+//    @Test
+    //runs forever
+//    public void checkCreatePlayer(){
+//        Scanner scanner = new Scanner(System.in);
+//        Board board = new Board();
+//        Game game = new Game(new Board());
+//        ArrayList<Player> playersList = new ArrayList<>();
+//        String playerName = "Random";
+//        InputStream in = new ByteArrayInputStream(playerName.getBytes());
+//        System.setIn(in);
+//        game.createPlayers(playersList,1, scanner);
+//    }
 }
